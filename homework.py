57770d8f4ddb9754a783ef5258a1ea94d88be0cc
Дрогаль Андрей <ddr533@yandex.ru>
2022-12-30 20:06:45 +0700

    fix mistakes

--- homework.py
+++ homework.py
@@ -120,7 +120,7 @@ class Swimming(Training):
                 * self.weight * self.duration)
 
 
-def read_package(workout_type: str, data: list[int]) -> Training:
+def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
     training_classes: dict[str, Training] = {'SWM': Swimming,
                                              'RUN': Running,
